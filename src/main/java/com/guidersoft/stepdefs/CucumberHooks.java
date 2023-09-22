c43bc8300e27a9d80d137ba3a113019515a55d8c
atezgider <atezgider@gmail.com>
2023-09-22 21:28:14 +0200

    22.09

--- src/main/java/com/guidersoft/stepdefs/CucumberHooks.java
+++ src/main/java/com/guidersoft/stepdefs/CucumberHooks.java
@@ -5,6 +5,11 @@ import io.cucumber.java.After;
 import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
 import io.cucumber.java.Scenario;
+import io.qameta.allure.Allure;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.ByteArrayInputStream;
 
 public class CucumberHooks {
 
@@ -18,7 +23,15 @@ public class CucumberHooks {
     @AfterStep
     public void afterStep(Scenario scenario){
         if (scenario.isFailed()){
+            byte[] resim = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            /*
+            // resim olarak kaydedilen byte[] i allurede teardown kismina ekler
+            Allure.addAttachment("Screenshot", new ByteArrayInputStream(resim));
+             */
 
+
+            scenario.attach(resim, "image/png", "Screenshot");
+            scenario.log(scenario.getName() + " is failed");
         }
     }
 
